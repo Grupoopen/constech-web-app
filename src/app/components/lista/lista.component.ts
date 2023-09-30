f047bedc16ca9f3155a282a5498aaa74f9e2c124
Enrique <u20201f172@upc.edu.pe>
2023-09-30 07:02:32 -0500

    Add-card-Crud
    a

---
+++ src/app/components/lista/lista.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/shared/task.service';
+import { Task } from '../../shared/listask.module';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+  tasks: Task[] = [];
+
+  newTask: Task = {
+    id: 0,
+    title: '',
+    description: ''
+  };
+
+  constructor(private listServ: TaskService) { }
+
+  ngOnInit(): void {
+    this.getTaskList();
+  }
+
+  getTaskList() {
+    this.listServ.getTasks().subscribe(
+      (res: any) => {
+        console.log(res); // PORQUE NO SALE AAA 
+        this.tasks = res;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+  
+  
+  
+//Agrega la card nueva 
+
+  addTask() {
+    console.log("agregado a la data", this.newTask);
+    this.listServ.addTask2(this.newTask).subscribe(() => {
+
+      this.getTaskList();
+    });
+
+    this.newTask = {
+      id: 0,
+      title: '',
+      description: ''
+    };
+  }
+
+
+  //elimina la card
+  deleteTask(id: number) {
+    this.listServ.deleteTask2(id).subscribe(() => {
+  
+      this.getTaskList();
+    
+    });
+  }
+}
--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -21,4 +24,36 @@ export class TaskService {
     return this._http.delete(`http://localhost:3000/task/${id}`);
   }
 
+  
+
+
+
+
+
+  getTasks():Observable<any>{
+    return this._http.get('http://localhost:3000/lista');
+  }
+  
+  /* getOneTask(_id:any){
+    return TaskList.find(task => task.id === _id)
+
+    
+  } */
+
+  addTask2(data:any): Observable<any>{
+    return this._http.post('http://localhost:3000/lista', data);
+  }
+/* 
+  editTask(newTaskEdit:any){
+    const index=TaskList.findIndex(task=>task.id ===newTaskEdit.id)
+
+    TaskList[index]=newTaskEdit;
+  }
+ */
+
+    deleteTask2(id:number):Observable<any>{
+      return this._http.delete(`http://localhost:3000/lista/${id}`);
+    }
+
+  
 }
